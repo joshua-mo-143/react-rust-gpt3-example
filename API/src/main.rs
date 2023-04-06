853603e850e4158cd145d0cf63cc22b0e1624541
Jeff Mitchell <sentinel1909@protonmail.com>
2023-04-05 20:16:37 -0700

    Update repo for version 0.13 of Shuttle

--- API/src/main.rs
+++ API/src/main.rs
@@ -1,21 +1,19 @@
 use shuttle_secrets::SecretStore;
 use std::path::PathBuf;
-use sync_wrapper::SyncWrapper;
 
 mod router;
 use router::handle_router;
 
-#[shuttle_service::main]
+#[shuttle_runtime::main]
 async fn axum(
     #[shuttle_secrets::Secrets] secrets: SecretStore,
     #[shuttle_static_folder::StaticFolder] static_folder: PathBuf,
-) -> shuttle_service::ShuttleAxum {
+) -> shuttle_axum::ShuttleAxum {
     let gpt3_token = secrets
         .get("GPT3_API_KEY")
         .expect("You need to set GPT3_API_KEY in your Secrets.toml file!");
 
     let router = handle_router(gpt3_token, static_folder);
-    let sync_wrapper = SyncWrapper::new(router);
 
-    Ok(sync_wrapper)
+    Ok(router.into())
 }
